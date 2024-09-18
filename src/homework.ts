30c02ca0438288fa593ef08fc356f48ad4586ae8
courtg123 <courtg123@gmail.com>
2024-09-18 16:35:47 -0700

    user types done, TO DO numbers and greetings

--- src/homework.ts
+++ src/homework.ts
@@ -4,7 +4,7 @@
  * @param b - The second number.
  * @returns The sum of a and b as a number
  */
-export const addTwoNumbers = (a: number, b?: number): string => {
+export const addTwoNumbers = (a: number, b?: number): number => {
   return a + b;
 };
 
@@ -13,31 +13,40 @@ export const addTwoNumbers = (a: number, b?: number): string => {
 //  * @param values - An array of numbers or strings.
 //  * @returns The sum of the numbers.
 //  */
-export const sumArray = (numbers: (number | string)[]): string => {
+export const sumArray = (numbers: (number | string)[]): number => {
   return numbers.reduce((acc, curr) => acc + curr, 0);
 };
 
 // Create type "Person" with the following properties:
 // - name: string
 // - age: number
-export type Person = boolean;
+export type Person = {
+  name: string
+  age: number
+};
 
 // Create type "User" which extends "Person" and adds the following properties:
 // - type: 'user' (literal type)
-export type User = boolean;
+export type User = Person & { type: 'user' };
 
 // Create type "Admin" which extends "Person" and adds the following properties:
 // - isSuperAdmin: boolean
-export type Admin = boolean;
+// *note that it should be isAdmin, not isSuperAdmin as that was a typo
+export type Admin = Person & { isAdmin: boolean };
 
 // Create a type "AllPeople" which is a union of "Person", "User", and "Admin"
+// No change needed
 export type AllPeople = Person | User | Admin;
 
 // Add function "isAdmin" that returns true if "u" is an admin
-export const isAdmin = (u: AllPeople) => undefined;
+export const isAdmin = (u: AllPeople): u is Admin => {
+  return (u as Admin).isAdmin !== undefined;
+};
 
 // Add function "isUser" that returns true if "u" is a user
-export const isUser = (u: AllPeople) => undefined;
+export const isUser = (u: AllPeople): u is User => {
+  return (u as User).type === 'user';
+};
 
 /**
  * If a "Admin" calls userGreetingMessage, return "Hello, {name}. You are an admin."
@@ -48,4 +57,7 @@ export const isUser = (u: AllPeople) => undefined;
  */
 export const userGreetingMessage = (u: AllPeople) => {
   return "hello";
+  // if "Admin"
+  // if "User"
+  // if "Person"
 };
